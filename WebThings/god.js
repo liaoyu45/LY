a4fa262f595f86fe6a44fd0c661027b1df202977
廖羽 <liaoyu45@163.com>
2016-10-26 15:03:46 +0800

    1

--- WebThings/god.js
+++ WebThings/god.js
@@ -412,6 +412,101 @@
                 }
             }
         };
+        this.color = (function () {
+            var innerClass = function () {
+                var reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;//http://www.zhangxinxu.com/wordpress/2010/03/javascript-hex-rgb-hsl-color-convert/
+                this.rgb2hex = function (rgb) {
+                    if (/^(rgb|RGB)/.test(rgb)) {
+                        var aColor = rgb.replace(/(?:\(|\)|rgb|RGB)*/g, "").split(",");
+                        var strHex = "#";
+                        for (var i = 0; i < aColor.length; i++) {
+                            var hex = Number(aColor[i]).toString(16);
+                            if (hex === "0") {
+                                hex += hex;
+                            }
+                            strHex += hex;
+                        }
+                        if (strHex.length !== 7) {
+                            strHex = rgb;
+                        }
+                        return strHex;
+                    } else if (reg.test(rgb)) {
+                        var aNum = rgb.replace(/#/, "").split("");
+                        if (aNum.length === 6) {
+                            return rgb;
+                        } else if (aNum.length === 3) {
+                            var numHex = "#";
+                            for (var i = 0; i < aNum.length; i += 1) {
+                                numHex += (aNum[i] + aNum[i]);
+                            }
+                            return numHex;
+                        }
+                    } else {
+                        return rgb;
+                    }
+                };
+                this.hex2rgb = function (hex) {
+                    hex = hex.toLowerCase();
+                    if (hex && reg.test(hex)) {
+                        if (hex.length === 4) {
+                            var sColorNew = "#";
+                            for (var i = 1; i < 4; i += 1) {
+                                sColorNew += hex.slice(i, i + 1).concat(hex.slice(i, i + 1));
+                            }
+                            hex = sColorNew;
+                        }
+                        var sColorChange = [];
+                        for (var i = 1; i < 7; i += 2) {
+                            sColorChange.push(parseInt("0x" + hex.slice(i, i + 2)));
+                        }
+                        return "RGB(" + sColorChange.join(",") + ")";
+                    } else {
+                        return hex;
+                    }
+                };
+                this.hsl2rgb = function (h, s, l) {//http://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion/
+                    var r, g, b;
+                    if (s == 0) {
+                        r = g = b = l;
+                    } else {
+                        var hue2rgb = function hue2rgb(p, q, t) {
+                            if (t < 0) t += 1;
+                            if (t > 1) t -= 1;
+                            if (t < 1 / 6) return p + (q - p) * 6 * t;
+                            if (t < 1 / 2) return q;
+                            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+                            return p;
+                        }
+                        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+                        var p = 2 * l - q;
+                        r = hue2rgb(p, q, h + 1 / 3);
+                        g = hue2rgb(p, q, h);
+                        b = hue2rgb(p, q, h - 1 / 3);
+                    }
+
+                    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+                };
+                this.rgb2hsl = function rgbToHsl(r, g, b) {
+                    r /= 255, g /= 255, b /= 255;
+                    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+                    var h, s, l = (max + min) / 2;
+                    if (max == min) {
+                        h = s = 0;
+                    } else {
+                        var d = max - min;
+                        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+                        switch (max) {
+                            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+                            case g: h = (b - r) / d + 2; break;
+                            case b: h = (r - g) / d + 4; break;
+                        }
+                        h /= 6;
+                    }
+                    return [h, s, l];
+                }
+            };
+            return new innerClass();
+        })();
         this.math = (function () {
             //get m from n, return indexes array, max index is n.
             function combination(n, m) {
