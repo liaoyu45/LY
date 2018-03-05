8973432d2d94b69520755ee8542cb21cc07ecf1c
liaoyu <liaoyu45@163.com>
2018-03-05 17:31:12 +0800

    1

--- AllOfMe/Global.asax.cs
+++ AllOfMe/Global.asax.cs
@@ -1,12 +1,10 @@
-﻿using BLL;
-using System;
+﻿using System;
 
 namespace AllOfMe {
 	public class Global : System.Web.HttpApplication {
 
 		protected void Application_Start(object sender, EventArgs e) {
-			Gods.Web.Him.Create<I>();
-			Gods.Web.Him.PreferJavascript();
+			Gods.Web.Him.Create<Global>();
 		}
 	}
 }
